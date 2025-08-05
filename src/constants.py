72db982b052f5d67e96129366e61e4eefd1cb31e
Yeva Hunanyan <yeva.hun@gmail.com>
2025-08-05 15:14:20 +0100

    Add experiments for V2 on small datasets up to 6 nodes

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,19 @@
 DEFAULT_WEIGHT = 0.5
 MEMORY_THRESHOLD_PERCENT = 90.0
+
+
+DAG_NODES_MAP = {'cancer': 5,
+                 'earthquake': 5,
+                 'survey': 6,
+                 'asia': 8,
+                 'sachs': 11,
+                 'child': 20,
+                 'insurance': 27}
+
+DAG_EDGES_MAP = {'cancer': 4,
+                 'earthquake': 4,
+                 'survey': 6,
+                 'asia': 8,
+                 'sachs': 17,
+                 'child': 25,
+                 'insurance': 52}
